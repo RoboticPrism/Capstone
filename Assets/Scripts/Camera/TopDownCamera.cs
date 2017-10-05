eb8e4be5cfe9948f93c90b7dbad191c332491afe
Ryan Stewart <stewart.rya@husky.neu.edu>
2017-10-05 10:33:23 -0400

    Top down controls and camera.  TODO: room warp to sidescroll levels and back.

---
+++ Assets/Scripts/Camera/TopDownCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCamera : MonoBehaviour {
+
+    public GameObject follow;
+    public float distanceFromPlayer; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(
+            follow.transform.position.x,
+            follow.transform.position.y,
+            distanceFromPlayer
+        );
+	}
+}
---
+++ Assets/Scripts/Player/TopDownPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownPlayer : MonoBehaviour {
+
+    GameObject playerSprite;
+    Rigidbody2D rb;
+    public float speed = 1f;
+
+	// Use this for initialization
+	void Start () {
+        playerSprite = transform.GetChild(0).gameObject;
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = new Vector2(
+            Input.GetAxis("Horizontal") * speed * Time.deltaTime, 
+            Input.GetAxis("Vertical") * speed * Time.deltaTime
+        );
+        if (rb.velocity.magnitude > .01f)
+        {
+            playerSprite.transform.rotation = Quaternion.RotateTowards(
+                playerSprite.transform.rotation,
+                Quaternion.AngleAxis(Mathf.Atan2(-rb.velocity.x, rb.velocity.y) * Mathf.Rad2Deg, Vector3.forward),
+                10f
+            );
+        }
+	}
+}
