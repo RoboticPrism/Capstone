0f08f0090a8a99b29088a504066fdbe0dff5d4df
Ryan Stewart <stewart.rya@husky.neu.edu>
2017-10-11 18:57:36 -0400

    Overworld controls + scene loading.

---
+++ Assets/Scripts/LevelStructure/SceneWarp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneWarp : MonoBehaviour {
+
+    public string sceneToLoad;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if ((col.GetComponent<PlayerMovement>() != null ||
+            col.GetComponent<TopDownPlayer>() != null) &&  
+            sceneToLoad != null)
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
