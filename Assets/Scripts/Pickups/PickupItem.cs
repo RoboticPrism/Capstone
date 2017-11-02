b0ba5a2e004b65dff4b90dd2c10fe46edbc00dca
Ryan Stewart <stewart.rya@husky.neu.edu>
2017-11-01 23:31:45 -0400

    Pickup system, UI for displaying picked up items.

---
+++ Assets/Scripts/Pickups/PickupItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupItem : MonoBehaviour {
+
+    public Sprite sprite;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
