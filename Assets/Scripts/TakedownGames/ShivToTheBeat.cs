fe2dfff1a801569921a6c6c8529f22c03ac51ff9
James Elliott <elliott.jame@husky.neu.edu>
2018-03-14 15:42:29 -0400

    initial additions to TDown game

--- Assets/Scripts/TakedownGames/ShivToTheBeat.cs
+++ Assets/Scripts/TakedownGames/ShivToTheBeat.cs
@@ -5,6 +5,24 @@ using UnityEngine.UI;
 
 public class ShivToTheBeat : TakedownGame {
 
+	public List<GameObject> possibleKeys;
+
+	private enum DIFFICULTY
+	{
+		EASY,
+		MEDIUM,
+		HARD,
+		MAKE_IT_STOP
+	};
+
+	private enum STYLE
+	{
+		RAIN,
+		SPIRAL,
+		SIDES,
+
+	};
+
 	// Use this for initialization
 	new void Start () {
 		
